64905a36c9bb00768506e4aff30e3fd22c8446ff
thelllounge <llearninglounge@gmail.com>
2021-11-20 00:57:12 +0900

    properly access data from csv

--- make_prompt.js
+++ make_prompt.js
@@ -11,8 +11,43 @@ var promptData
 
 // };
 
-const prompt = document.getElementById("prompt");
+// const prompt = document.getElementById("prompt");
 
-document.getElementById('get_prompt').onclick = function(){
-    prompt.innerHTML = `<p>You should write a story </p>`
-};
+// document.getElementById('get_prompt').onclick = function(){
+//     prompt.innerHTML = `<p>You should write a story </p>`
+// };
+
+/* Stuff I was messing with on replit updated with these IDs*/
+
+var promptData
+
+Papa.parse('https://docs.google.com/spreadsheets/d/e/2PACX-1vT3pJgyMkUFc6wet1PPnyJ4AquHwmvP0NYb6rx5z1N59TngIUnqP9N5ZGLJpIGiQlF3ItQwtn5kEdYc/pub?gid=0&single=true&output=csv', {
+  download: true,
+  header: true,
+  error: undefined,
+  complete: function(results) {
+    console.log(results)
+    //results.data.map((data, index)=>{
+    //  console.log(results);
+    //  promptData = results;
+    //});
+    promptData = results;
+    console.log(promptData.data[Math.floor(Math.random()*5)]['Genre']);
+    console.log(promptData.data[Math.floor(Math.random()*5)]['Genre']);
+    console.log(promptData.data[Math.floor(Math.random()*5)]['Genre']);
+    console.log(promptData.data[Math.floor(Math.random()*5)]['Genre']);
+    console.log(promptData.data[Math.floor(Math.random()*5)]['Genre']);
+  }
+})
+
+//console.log(promptData);
+let promptButton = document.getElementById('getPrompt'); 
+let promptPara = document.getElementById('prompt'); 
+
+promptButton.onclick = function(){
+  promptPara.innerHTML =  "hello";
+}
+
+
+
+//console.log(Math.floor(Math.random()*5));
