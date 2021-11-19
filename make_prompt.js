4209610e69eccb687c18bfaafb4d9f7afdf4add8
thelllounge <llearninglounge@gmail.com>
2021-11-19 23:38:16 +0900

    got rid of grid stuff and something else who knows

--- make_prompt.js
+++ make_prompt.js
@@ -1,10 +1,18 @@
-//it looks like an easy was to affect one page from another is using jquery and ajax so I need to learn that.
-fetch('prompt.json')
-.then(Response => Response.json())
-.then(data => console.log(data));
+var promptData
 
 
+
+// window.onload = function(){
+// fetch('prompt.json')
+// .then(Response => Response.json())
+// .then(data => document.getElementById('get_prompt').onclick = function(){
+//     prompt.innerHTML = `<p>You should write a story in ${data.Setting[2]}</p>`
+// });
+
+// };
+
+const prompt = document.getElementById("prompt");
 
 document.getElementById('get_prompt').onclick = function(){
-    document.getElementById("prompt").innerHTML = `<p>Shit, I did it!</p>`
+    prompt.innerHTML = `<p>You should write a story </p>`
 };
