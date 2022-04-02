ce5a580c9607665f307a429dec704983325ee01c
thelllounge <llearninglounge@gmail.com>
2022-04-02 17:14:28 +0900

    removed date, added padstart, worked on display

--- sprint.js
+++ sprint.js
@@ -1,28 +1,30 @@
-var currentTime = Date.now();
-
-console.log(currentTime);
-
-
-
-var currentTimeReadable = new Date(currentTime);
-
+let sprintSelectString = document.getElementById("select"); //this doesn't select the value.
 
 function saveTime(){
-    let timerTime = new Date(currentTime + (Number(document.getElementById("select").value) * 60000));
+    let timerTime;
+
+    if(sprintSelectString.value == "Select a sprint time" && document.getElementById("rest").value == "Select a rest time"){
+        document.getElementById("timeDisplay").innerHTML = "<p>Please select a sprint and rest time.</p>"
+    }else if(sprintSelectString.value == "Select a sprint time"){
+        document.getElementById("timeDisplay").innerHTML = "<p>Please select a sprint time.</p>"
+    }else if(document.getElementById("rest").value == "Select a rest time"){
+        document.getElementById("timeDisplay").innerHTML = "<p>Please select a rest time.</p>"
+    }else{
+        // timerTime = new Date(currentTime + (Number(document.getElementById("select").value) * 60000));
+        let sprintMinutes = sprintSelectString.value.padStart(2, "0");
+        document.getElementById("timeDisplay").innerHTML = `<p>00:${sprintMinutes}:00</p>`;
+    }
+
+    // if(document.getElementById("select").value != "Select a sprint time" && document.getElementById("rest").value != "Select a rest time"){
+        
+    // }
+    
+
+    // console.log(sprintValueString.value)
+    
 
 
-    console.log(timerTime);
-
-
-    //This is properly printing the difference between the current time and adding the proper amount in minutes from the sprint duration chosen.
-    //However, when the minutes are a single digit it doesn't add the first 0 to make it look nice (so 1 minute is 0:1:0) this is a problem with seconds as well.
-    //I also need to figure out how to do the actual countdown part as well.
-    //Gotta figure out how to add sounds and restart the countdown with the rest without having to do anything.
-    let countDownHours = timerTime.getHours() - currentTimeReadable.getHours();
-    let countDownMinutes = timerTime.getMinutes() - currentTimeReadable.getMinutes();
-    let countDownSeconds = timerTime.getSeconds() - currentTimeReadable.getSeconds();
-
-    document.getElementById("timeDisplay").innerHTML = `<p>${countDownHours}:${countDownMinutes}:${countDownSeconds}</p>`;
+    // document.getElementById("timeDisplay").innerHTML = `<p>00:${sprintMinutes}:00</p>`;
 }
 
 //not 100% sure where I'm going with this but these wsill give me the time in millisends and then convert it to a readable time. 
