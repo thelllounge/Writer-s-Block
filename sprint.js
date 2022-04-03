1379aa2f2341fa68dcd2964e407afb9942bf9ae0
thelllounge <llearninglounge@gmail.com>
2022-04-04 01:04:53 +0900

    worked on timer. Countdown works. Pause no

--- sprint.js
+++ sprint.js
@@ -1,30 +1,62 @@
-let sprintSelectString = document.getElementById("select"); //this doesn't select the value.
+//Grabbing things by ID from the HTML
+let sprintSelect = document.getElementById("select");
+let restSelect = document.getElementById("rest");
+let startButton = document.getElementById("startButton");
+let pauseButton = document.getElementById("pauseButton");
 
+//Adding functions to actions
+sprintSelect.addEventListener("change", saveTime);
+restSelect.addEventListener("change", saveTime);
+
+startButton.addEventListener("click", startTimer);
+// pauseButton.addEventListener("click", pauseTheTimer);
+
+//Needed variables to make the functions work
+let sprintTime = 0;
+let restTime = 0;
+
+let sprinting = false;
+
+
+//This makes sure that both a sprint time and a rest time are selected, and if not reminds people to select what's missing.
+//Once both are selected it displays the sprint time on the screen.
 function saveTime(){
-    let timerTime;
 
-    if(sprintSelectString.value == "Select a sprint time" && document.getElementById("rest").value == "Select a rest time"){
+    if(sprintSelect.value == "Select a sprint time" && document.getElementById("rest").value == "Select a rest time"){
         document.getElementById("timeDisplay").innerHTML = "<p>Please select a sprint and rest time.</p>"
-    }else if(sprintSelectString.value == "Select a sprint time"){
+    }else if(sprintSelect.value == "Select a sprint time"){
         document.getElementById("timeDisplay").innerHTML = "<p>Please select a sprint time.</p>"
     }else if(document.getElementById("rest").value == "Select a rest time"){
         document.getElementById("timeDisplay").innerHTML = "<p>Please select a rest time.</p>"
     }else{
-        // timerTime = new Date(currentTime + (Number(document.getElementById("select").value) * 60000));
-        let sprintMinutes = sprintSelectString.value.padStart(2, "0");
+        let sprintMinutes = sprintSelect.value.padStart(2, "0");
         document.getElementById("timeDisplay").innerHTML = `<p>00:${sprintMinutes}:00</p>`;
+        sprintTime = Number(sprintSelect.value * 60);
+        restTime = Number(restSelect.value * 60);
     }
-
-    // if(document.getElementById("select").value != "Select a sprint time" && document.getElementById("rest").value != "Select a rest time"){
-        
-    // }
-    
-
-    // console.log(sprintValueString.value)
-    
-
-
-    // document.getElementById("timeDisplay").innerHTML = `<p>00:${sprintMinutes}:00</p>`;
 }
 
-//not 100% sure where I'm going with this but these wsill give me the time in millisends and then convert it to a readable time. 
+function startTimer(){
+    if(sprintSelect.value == "Select a sprint time" || document.getElementById("rest").value == "Select a rest time"){
+        document.getElementById("timeDisplay").innerHTML = "<p>Please select both a sprint and rest time.</p>"
+    }
+
+    const timerCountdown = setInterval(countDownSprint, 1000);
+    startButton.innerHTML = 'Pause';
+    
+    if(sprinting == true){
+        clearInterval(timerCountdown);
+        sprinting == false;
+        startButton.innerHTML = "Start"
+    }
+
+    sprinting = true;
+
+}
+
+
+
+function countDownSprint(){
+    sprintTime--
+    document.getElementById("timeDisplay").innerHTML = `<p>00:${String(Math.floor(sprintTime / 60)).padStart(2, "0")}:${String(sprintTime % 60).padStart(2,"0")}</p>`;
+}
